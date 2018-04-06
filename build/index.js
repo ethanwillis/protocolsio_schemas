826f3beef69d087d140bccd5d8e9cb174462d848
Ethan Willis <elwillis1@gmail.com>
2018-04-06 11:35:49 -0500

    Updated package build scripts and main js file.
    Added index.js file that exports all objects/schemas from this package.

---
+++ build/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.Case = exports.Step = exports.SmallProtocol = exports.Protocol = exports.Reagent = exports.Image = exports.User = exports.Comment = exports.StepComponentType26 = exports.StepComponentType25 = exports.StepComponentType24 = exports.StepComponentType22 = exports.StepComponentType21 = exports.StepComponentType20 = exports.StepComponentType19 = exports.StepComponentType18 = exports.StepComponentType17 = exports.StepComponentType15 = exports.StepComponentType13 = exports.StepComponentType9 = exports.StepComponentType8 = exports.StepComponentType7 = exports.StepComponentType6 = exports.StepComponentType4 = exports.StepComponentType3 = exports.StepComponentType1 = undefined;
+
+var _StepComponentType = require('./schemas/StepComponentType');
+
+var _Comment = require('./schemas/Comment');
+
+var _User = require('./schemas/User');
+
+var _Image = require('./schemas/Image');
+
+var _Reagent = require('./schemas/Reagent');
+
+var _Protocol = require('./schemas/Protocol');
+
+var _SmallProtocol = require('./schemas/SmallProtocol');
+
+var _Step = require('./schemas/Step');
+
+var _StepComponent = require('./schemas/StepComponent');
+
+var _Case = require('./schemas/Case');
+
+exports.StepComponentType1 = _StepComponentType.StepComponentType1;
+exports.StepComponentType3 = _StepComponentType.StepComponentType3;
+exports.StepComponentType4 = _StepComponentType.StepComponentType4;
+exports.StepComponentType6 = _StepComponentType.StepComponentType6;
+exports.StepComponentType7 = _StepComponentType.StepComponentType7;
+exports.StepComponentType8 = _StepComponentType.StepComponentType8;
+exports.StepComponentType9 = _StepComponentType.StepComponentType9;
+exports.StepComponentType13 = _StepComponentType.StepComponentType13;
+exports.StepComponentType15 = _StepComponentType.StepComponentType15;
+exports.StepComponentType17 = _StepComponentType.StepComponentType17;
+exports.StepComponentType18 = _StepComponentType.StepComponentType18;
+exports.StepComponentType19 = _StepComponentType.StepComponentType19;
+exports.StepComponentType20 = _StepComponentType.StepComponentType20;
+exports.StepComponentType21 = _StepComponentType.StepComponentType21;
+exports.StepComponentType22 = _StepComponentType.StepComponentType22;
+exports.StepComponentType24 = _StepComponentType.StepComponentType24;
+exports.StepComponentType25 = _StepComponentType.StepComponentType25;
+exports.StepComponentType26 = _StepComponentType.StepComponentType26;
+exports.Comment = _Comment.Comment;
+exports.User = _User.User;
+exports.Image = _Image.Image;
+exports.Reagent = _Reagent.Reagent;
+exports.Protocol = _Protocol.Protocol;
+exports.SmallProtocol = _SmallProtocol.SmallProtocol;
+exports.Step = _Step.Step;
+exports.Case = _Case.Case;
