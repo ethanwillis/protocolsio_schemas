8a6ecf627463fa32a16736db3ec2b087c0d47e9b
Ethan Willis <elwillis1@gmail.com>
2018-04-08 08:52:52 -0500

    Fixed Step component test object. Updated test object naming scheme. Updated assertions to have better error message output

--- src/test/validate_step.js
+++ src/test/validate_step.js
@@ -2,26 +2,31 @@ import { ProtocolsIOValidator } from './ProtocolsIOValidator'
 import { Step } from '../schemas/Step';
 var assert = require('chai').assert;
 
-let s = {
+let s1_valid = {
   "id": 595209,
   "guid": "E70FDFE632504ADFB0ED519ABB5449B1",
   "previous_id": 0,
   "previous_guid": null,
   "modified_on": 1517933242,
-  "components": [{
-    "id": 1023444,
-    "guid": "A38362CBC954458FB069F821B6526B38",
-    "previous_id": 1023443,
-    "previous_guid": "1EBCBC24EFCF429F8F34D7099EF6211E",
-    "type_id": 3,
-    "title": "Amount"
-  }]
+  "components": [
+    {
+      "id": 1023444,
+      "guid": "A38362CBC954458FB069F821B6526B38",
+      "order_id": 1,
+      "type_id": 1,
+      "title": "Description",
+      "source": {
+        "description": "<p>step to make smth</p>"
+      }
+    }
+  ]
 }
 
 describe('Step validator', function() {
-    describe('#validate()', function() {
-	it('should return no errors when a valid step object is passed in', function() {
-	    assert.lengthOf((ProtocolsIOValidator.validate(s, Step))['errors'], 0);
-	});
+  describe('#validate()', function() {
+    it('should return no errors when a valid step object is passed in', function() {
+      let validator_result = ProtocolsIOValidator.validate(s1_valid, Step);
+      assert.lengthOf(validator_result['errors'], 0, validator_result['errors']);
     });
+  });
 });
