d66feb45e3381034b7e35ca6d622c6db50f06798
Ethan Willis <elwillis1@gmail.com>
2018-04-08 08:15:26 -0500

    Updated test for User object to have better error message output. Also fixed broken test object.

--- src/test/validate_user.js
+++ src/test/validate_user.js
@@ -3,22 +3,21 @@ import { User } from '../schemas/User';
 var assert = require('chai').assert;
 
 let u1_valid = {
-    "user": {
-      "name": "Vladimir Frolov",
-      "affiliation": null,
-      "username": "vladimir-frolov10",
-      "link": null,
-      "image": {
-        "source": "https://s3.amazonaws.com/pr-journal/djqbjf6.jpg",
-        "placeholder": "https://s3.amazonaws.com/pr-journal/djqbjf6.jpg"
-      }
-    }
+  "name": "Vladimir Frolov",
+  "affiliation": null,
+  "username": "vladimir-frolov10",
+  "link": null,
+  "image": {
+    "source": "https://s3.amazonaws.com/pr-journal/djqbjf6.jpg",
+    "placeholder": "https://s3.amazonaws.com/pr-journal/djqbjf6.jpg"
+  }
 }
 
 describe('User validator', function() {
     describe('#validate()', function() {
     	it('should return no errors when a valid user object is passed in', function() {
-    	    assert.lengthOf((ProtocolsIOValidator.validate(u1_valid, User))['errors'], 0);
+        let validator_result = ProtocolsIOValidator.validate(u1_valid, User);
+        assert.lengthOf(validator_result['errors'], 0, validator_result['errors']);
     	});
     });
 });
