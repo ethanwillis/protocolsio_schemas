348841129aa7288e45541686edd5424aae2e83ae
Ethan Willis <elwillis1@gmail.com>
2018-04-08 09:04:30 -0500

    For Reagent object Tests: - Updated naming scheme for test objects - Made error messages more informative

--- src/test/validate_reagent.js
+++ src/test/validate_reagent.js
@@ -2,7 +2,7 @@ import { ProtocolsIOValidator } from './ProtocolsIOValidator'
 import { Reagent } from '../schemas/Reagent';
 var assert = require('chai').assert;
 
-let r = {
+let r1_valid = {
   "id": 19751,
   "mol_weight": 0,
   "name": "2 mg Gastrin I, human",
@@ -24,7 +24,8 @@ let r = {
 describe('Reagent validator', function() {
     describe('#validate()', function() {
 	it('should return no errors when a valid reagent object is passed in', function() {
-	    assert.lengthOf((ProtocolsIOValidator.validate(r, Reagent))['errors'], 0);
+    let validator_result = ProtocolsIOValidator.validate(r1_valid, Reagent);
+    assert.lengthOf(validator_result['errors'], 0, validator_result['errors']);
 	});
     });
 });
