558f53752ea1f1d9c7893e2b3cd51bc6ed2109b6
Ethan Willis <elwillis1@gmail.com>
2018-04-08 09:00:16 -0500

    For Image object Tests: - Updated naming scheme. - Fixed test input object to be valid. - Updated error messages to be more informative.

--- src/test/validate_image.js
+++ src/test/validate_image.js
@@ -3,17 +3,16 @@ import { Image } from '../schemas/Image';
 var assert = require('chai').assert;
 
 
-let i = {
-    "image": {
-        "source": "https://www.protocols.io/img/default_protocol.png",
-        "placeholder": "https://www.protocols.io/img/default_protocol.png"
-    }
+let i1_valid = {
+  "source": "https://www.protocols.io/img/default_protocol.png",
+  "placeholder": "https://www.protocols.io/img/default_protocol.png"
 };
 
 describe('Image validator', function() {
-    describe('#validate()', function() {
-	it('should return no errors when a valid object is passed in', function() {
-	    assert.lengthOf((ProtocolsIOValidator.validate(i, Image))['errors'], 0);
-	});
-    });
+  describe('#validate()', function() {
+    it('should return no errors when a valid object is passed in', function() {
+      let validator_result = ProtocolsIOValidator.validate(i1_valid, Image);
+      assert.lengthOf(validator_result['errors'], 0, validator_result['errors']);
+	  });
+  });
 });
